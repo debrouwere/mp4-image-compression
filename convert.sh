2fc002b1c9655389f1b5f469c8271a0d5452c1b9
Stijn Debrouwere <stijn@stdout.be>
2013-04-13 12:03:47 +0100

    Add some additional quality settings to the mp4.

--- convert.sh
+++ convert.sh
@@ -1,17 +1,35 @@
 mkdir -p {intermediates,output}
 echo 'Converting PNG files into MP4. Takes about a minute, so some patience.'
 convert data/bbc-*.png -extent 1024x4000 intermediates/crop-%02d.png
-# -qscale:v 0 is the best quality we can get
-# it's not lossless like PNG is, and the quality isn't perfect, 
-# but it's reasonable and text is legible
-ffmpeg -framerate 1 -i intermediates/crop-%02d.png -c:v libx264 -qscale:v 0 output/bbc.mp4
 # generate jpg and webp, and show file size of each
 convert intermediates/crop-00.png output/still.webp
 convert intermediates/crop-00.png -quality 100% output/still-q100.jpg
 convert intermediates/crop-00.png -quality 80% output/still-q80.jpg
 convert intermediates/crop-00.png -quality 60% output/still-q60.jpg
+pngquant --ext -quant.png intermediates/crop-00.png
+mv intermediates/crop-00-quant.png output/still-quant.png
+
+for quality in 1 10 20 30 40
+do
+    ffmpeg \
+        -framerate 1 \
+        -i intermediates/crop-%02d.png \
+        -c:v libx264 \
+        -crf $quality \
+        -preset slow \
+        output/bbc-q$quality.mp4
+done
+
 du -skh output/*
-du -k output/bbc.mp4 | cut -f1 | xargs -I {} expr {} / 24 | xargs -I {} echo "{}K    size per image in output/bbc.mp4"
+
+for quality in 1 10 20 30 40
+do
+    du -k output/bbc-q$quality.mp4 | 
+    cut -f1 | 
+    xargs -I {} expr {} / 24 | 
+    xargs -I {} echo "{}K    size per image in output/bbc-$quality.mp4"
+done
+
 # show example
 jade show.jade -O output
 coffee -co output show.coffee
--- show.coffee
+++ show.coffee
@@ -1,11 +1,29 @@
+load = (q) ->
+    src = "bbc-#{q}.mp4"
+    source = $("<source/>")
+    source.attr 'src', src
+    $("#video").html source
+    Popcorn "#video"
+
 $(document).ready ->
     for i in [0..23]
         option = $("<option/>")
         option.text "#{i}:00 hr"
         option.attr 'value', i
-        $("select").append option
+        $("#time").append option
 
-    video = Popcorn "#video"
+    for q in ['q1', 'q10', 'q20', 'q30', 'q40']
+        option = $("<option/>")
+        option.text "CRF #{q}"
+        option.attr 'value', q
+        $("#quality").append option       
 
-    $("select").change ->
+    video = load('q1')
+
+    $("#time").change ->
         video.currentTime $(this).val()
+
+    $("#quality").change ->
+        video = load $(this).val()
+        video.on 'canplay', ->
+            video.currentTime $("#time").val()
